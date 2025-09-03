991f197aaf299ed1cefac7f381f8895c9c72b7d7
feiyu <w_feiyu@outlook.com>
2025-09-04 02:23:53 +0800

    Basic command to manage event details

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.3.0] - 2025-09-04
+### Added
+- Java 21
+- `/eventstatus` to expose event details tracked
+- `/manageevent` to allow edit event details tracked
+
 ## [1.2.2] - 2025-08-30
 ### Fixes
 - Deleted event post removed from promo message
--- src/main/java/com/feiyu/discord/sg/tavern/services/CommandService.java
+++ src/main/java/com/feiyu/discord/sg/tavern/services/CommandService.java
@@ -6,11 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -34,6 +38,21 @@ public class CommandService {
         // mod commands
         guild.upsertCommand("invite", "Get an invite link to this server (valid 48hrs)")
                 .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.CREATE_INSTANT_INVITE))
+                .queue();
+        
+        // event command
+        guild.upsertCommand("eventstatus", "Update event postStatus (MANAGED/NEW/PAST/CANCELLED/TBC)")
+                //.addOptions(List.of(new OptionData(OptionType.STRING, "poststatus", "Update Post Status", false)))
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_SERVER))
+                .queue();
+        
+        guild.upsertCommand("manageevent", "Update event processedEventName")
+                .addOptions(List.of(
+                        new OptionData(OptionType.STRING, "eventname", "Event Name", false),
+                        new OptionData(OptionType.STRING, "eventlocation", "Event Location", false),
+                        new OptionData(OptionType.STRING, "eventdatetime", "Event DateTime", false)
+                ))
+                .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_SERVER))
                 .queue();
         
         // general commands
